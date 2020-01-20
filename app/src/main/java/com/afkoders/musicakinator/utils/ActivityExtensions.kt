cce599fd147bc079658756639c99e5e8527370fa
Yaroslav Ovdiienko <yaroslav@Yaroslavs-MBP-2.local>
2020-01-21 00:19:30 +0200

    - add Activity extensions - add Android extensions - add Button extensions - add TextView extensions - add View extensions

---
+++ app/src/main/java/com/afkoders/musicakinator/utils/ActivityExtensions.kt
@@ -0,0 +1,23 @@
+package com.afkoders.musicakinator.utils
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+
+
+inline fun <reified T : Activity> Activity.openActivity(initializer: Intent.() -> Unit = {}) {
+    val intent = Intent(this, T::class.java).apply { initializer() }
+    startActivity(intent)
+}
+
+inline fun <reified T : Activity> Activity.openActivityForResult(
+    requestCode: Int,
+    options: Bundle? = null,
+    block: Intent.() -> Unit = {}
+) {
+    startActivityForResult(
+        Intent(this, T::class.java).apply { block.invoke(this) },
+        requestCode,
+        options
+    )
+}
