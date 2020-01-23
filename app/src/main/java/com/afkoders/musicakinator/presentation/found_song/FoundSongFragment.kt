db07a54ac533545367534e598a4d420de8776f43
kalevych_oleksandr <kalevych.oleksandr@uptech.team>
2020-01-23 18:43:07 +0200

    deezer found song and show it to user

--- app/src/main/java/com/afkoders/musicakinator/presentation/found_song/FoundSongFragment.kt
+++ app/src/main/java/com/afkoders/musicakinator/presentation/found_song/FoundSongFragment.kt
@@ -1,14 +1,19 @@
 package com.afkoders.musicakinator.presentation.found_song
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.lifecycle.ViewModelProviders
 import com.afkoders.musicakinator.R
 import com.afkoders.musicakinator.presentation.BaseFragment
 import com.afkoders.musicakinator.utils.extensions.makeGone
 import com.afkoders.musicakinator.utils.extensions.makeVisible
+import com.deezer.sdk.model.PaginatedList
+import com.deezer.sdk.model.Track
+import com.deezer.sdk.network.request.event.JsonRequestListener
 import kotlinx.android.synthetic.main.fragment_found_song.*
 import timber.log.Timber
+import java.lang.Exception
 
 
 /**
@@ -22,17 +27,27 @@ class FoundSongFragment : BaseFragment<FoundSongViewModel>(R.layout.fragment_fou
 
         arguments?.getString("lyrics").let {
             loadingBackgorund.makeVisible()
-            viewModel.searchArtist()
-                .subscribe { result
-                             , err ->
-                    loadingBackgorund.makeGone()
+            viewModel.searchArtist(it?:"")
+                .subscribe { result, err ->
+                    viewModel.searchTrackByName(requireActivity().applicationContext, result.get(0).artist, result.get(0).title,
+                        object : JsonRequestListener() {
+                            override fun onResult(p0: Any?, p1: Any?) {
+                                Log.d("DeezerLog", "result = "+ p0.toString())
+                                val track = p0 as PaginatedList<Track>
+                                Log.d("DeezerLog", "result title = "+ track[0].title)
+                                loadingBackgorund.makeGone()
+                                tvFoundedSongTitle.text = track[0].title
+                            }
+
+                            override fun onUnparsedResult(p0: String?, p1: Any?) {
+                            }
+
+                            override fun onException(p0: Exception?, p1: Any?) {
+                            }
+                        })
                     Timber.d("FUCK. result: $result")
                 }
         }
-    }
-
-    private fun setupLoadingView() {
-
     }
 
     override fun provideViewModel(): FoundSongViewModel =
