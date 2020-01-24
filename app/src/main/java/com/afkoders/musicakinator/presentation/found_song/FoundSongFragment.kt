2dae4907ff9b66202631064b4c3b4e6748f378f2
kalevych_oleksandr <kalevych.oleksandr@uptech.team>
2020-01-24 16:54:02 +0200

    first

--- app/src/main/java/com/afkoders/musicakinator/presentation/found_song/FoundSongFragment.kt
+++ app/src/main/java/com/afkoders/musicakinator/presentation/found_song/FoundSongFragment.kt
@@ -8,7 +8,16 @@ import com.afkoders.musicakinator.R
 import com.afkoders.musicakinator.presentation.BaseFragment
 import com.afkoders.musicakinator.presentation.interation_with_akinator.Interaction
 import com.afkoders.musicakinator.presentation.interation_with_akinator.InteractionViewModel
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.SimpleExoPlayer
 import kotlinx.android.synthetic.main.fragment_found_song.*
+import sun.audio.AudioPlayer.player
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.util.Util.getUserAgent
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import android.R
+import com.google.android.exoplayer2.util.Util
 
 
 /**
@@ -52,6 +61,21 @@ class FoundSongFragment : BaseFragment<InteractionViewModel>(R.layout.fragment_f
 
         btnSong.chipBackgroundColor = ColorStateList(states, colors)
         btnLyrics.chipBackgroundColor = ColorStateList(states, colors)
+
+        val player = SimpleExoPlayer.Builder(requireContext()).build()
+        playerView.player = player
+
+        // Produces DataSource instances through which media data is loaded.
+        val dataSourceFactory = DefaultDataSourceFactory(
+            context,
+            Util.getUserAgent(requireContext(), "yourApplicationName")
+        )
+// This is the MediaSource representing the media to be played.
+        val videoSource = ProgressiveMediaSource.Factory(dataSourceFactory)
+            .createMediaSource(mp4VideoUri)
+// Prepare the player with the source.
+        player.prepare(videoSource)
+
     }
 
     override fun setupOutputs() {
