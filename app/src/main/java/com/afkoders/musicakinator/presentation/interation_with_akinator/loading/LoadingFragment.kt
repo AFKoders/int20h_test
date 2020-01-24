f04230c9ed385efc65c41aa12935c021c8437a2b
Yaroslav Ovdiienko <uptechandroidteam@Yaroslavs-MacBook-Pro.local>
2020-01-24 13:57:51 +0200

    add add args for loading - fix error with compiled sources with java 6 instead of java 8

--- app/src/main/java/com/afkoders/musicakinator/presentation/interation_with_akinator/loading/LoadingFragment.kt
+++ app/src/main/java/com/afkoders/musicakinator/presentation/interation_with_akinator/loading/LoadingFragment.kt
@@ -1,11 +1,14 @@
 package com.afkoders.musicakinator.presentation.interation_with_akinator.loading
 
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.navArgs
 import com.afkoders.musicakinator.R
 import com.afkoders.musicakinator.presentation.BaseFragment
 import com.afkoders.musicakinator.presentation.interation_with_akinator.InteractionViewModel
 
 class LoadingFragment : BaseFragment<InteractionViewModel>(R.layout.fragment_loading) {
+
+    val args: LoadingFragmentArgs by navArgs()
 
     override fun provideViewModel() =
         ViewModelProviders.of(requireActivity(), viewModelFactory)[InteractionViewModel::class.java]
@@ -15,12 +18,17 @@ class LoadingFragment : BaseFragment<InteractionViewModel>(R.layout.fragment_loa
     }
 
     override fun setupOutputs() {
-        // TODO get argument with lyrics
+        args.lyrics?.let {
+            viewModel.searchSongs(it)
+                .subscribe { intermixes, err ->
+                    viewModel.reset()
+                    viewModel.putItems(intermixes)
+                }.disposeByBagProvider()
+        } ?: goToSearch()
+    }
 
-        viewModel.searchSongs("")
-            .subscribe { intermixes, err ->
-                // TODO for Yaroslav process somehow with router
-            }.disposeByBagProvider()
+    private fun goToSearch() {
+        // TODO: add direction to search screen
     }
 
 }
