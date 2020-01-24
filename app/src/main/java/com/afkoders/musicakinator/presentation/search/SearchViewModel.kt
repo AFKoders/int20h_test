89e5a55cdd15f37eb3a3d733e6f48ce4a4887e9a
Andrii Chernysh <afkoders@gmail.com>
2020-01-24 11:58:38 +0200

    Added Intermix model creation

--- app/src/main/java/com/afkoders/musicakinator/presentation/search/SearchViewModel.kt
+++ app/src/main/java/com/afkoders/musicakinator/presentation/search/SearchViewModel.kt
@@ -2,12 +2,12 @@ package com.afkoders.musicakinator.presentation.search
 
 import androidx.lifecycle.ViewModel
 import com.afkoders.musicakinator.data.models.DeezerSong
+import com.afkoders.musicakinator.data.models.Song
 import com.afkoders.musicakinator.data.repository.AuddRepository
 import com.afkoders.musicakinator.data.repository.DeezerRepository
 import com.afkoders.musicakinator.di.qualifiers.SchedulerIO
 import com.afkoders.musicakinator.di.qualifiers.SchedulerUI
-import com.afkoders.musicakinator.presentation.interation_with_akinator.IntermixModel
-import com.afkoders.musicakinator.presentation.interation_with_akinator.IntermixModelImpl
+import com.afkoders.musicakinator.presentation.interation_with_akinator.Intermix
 import io.reactivex.Scheduler
 import io.reactivex.Single
 import javax.inject.Inject
@@ -19,23 +19,47 @@ class SearchViewModel @Inject constructor(
     @SchedulerUI private val schedulerUI: Scheduler,
     @SchedulerIO private val schedulerIO: Scheduler
 ) : ViewModel() {
-    fun searchSongs(lyrics: String): Single<List<IntermixModelImpl>> =
+    fun searchSongs(lyrics: String): Single<List<Intermix>> =
         auddRepository
             .findSongsByLyrics(lyrics)
             .flatMap {
-                val deezerObservablesList: List<Single<DeezerSong>> = it.take(5)
-                    .map { song ->
-                        deezerRepository.findSongByTitle(song.fullTitle ?: song.title)
-                            .onErrorReturnItem(DeezerSong())
-                    }
-
-                // TODO AC. Create valid Intermix model.
+                val deezerObservablesList: List<Single<Pair<Song, DeezerSong>>> = it.take(5)
+                    .map { song -> mapToSongAndDeezerSong(song) }
 
                 Single.zip(deezerObservablesList) { arr: Array<Any> ->
-                    arr.toList().map { it as DeezerSong }.filter { !it.title.isNullOrBlank() }
-                        .map { IntermixModelImpl(false, 0, "", "", "", "") }
+                    arr.toList()
+                        .map { it as Pair<Song, DeezerSong> }
+                        .filter { !it.first.songId.isNullOrBlank() }
+                        .map { (song, deezerSong) -> createIntermixModel(song, deezerSong) }
                 }
             }
             .subscribeOn(schedulerIO)
             .observeOn(schedulerUI)
+
+    private fun createIntermixModel(
+        song: Song,
+        deezerSong: DeezerSong
+    ): Intermix {
+        return Intermix(
+            deezerSong.link ?: "",
+            song.songId?.toInt() ?: 0,
+            deezerSong.title ?: "",
+            song.lyrics ?: "",
+            deezerSong.album?.picture ?: "",
+            song.artist ?: ""
+        )
+    }
+
+    private fun mapToSongAndDeezerSong(song: Song): Single<Pair<Song, DeezerSong>> {
+        val songName = song.fullTitle ?: song.title
+        val defaultValue = Song() to DeezerSong()
+
+        return if (!songName.isNullOrBlank()) {
+            deezerRepository.findSongByTitle(songName)
+                .map { song to it }
+                .onErrorReturnItem(defaultValue)
+        } else {
+            Single.just(defaultValue)
+        }
+    }
 }
